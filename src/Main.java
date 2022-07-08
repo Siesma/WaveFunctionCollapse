663ed5446379b3792840e07b43fe2b1f2ad49b62
Tom <tom.rodewald@stud.unibas.ch>
2022-07-08 12:02:06 +0200

    Creation of the repository. Processing hook already installed

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+
+  public static void main(String[] args) {
+    PApplet.main("Main.java", args);
+  }
+
+
+  @Override
+  public void draw () {
+
+  }
+
+  @Override
+  public void setup () {
+
+  }
+
+  @Override
+  public void settings () {
+
+  }
+
+}
