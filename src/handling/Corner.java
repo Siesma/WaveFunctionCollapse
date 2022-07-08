8bebc2aa5ee18ad6503d22342b0f6fa788b80116
Tom <tom.rodewald@stud.unibas.ch>
2022-07-08 12:18:21 +0200

    Implemented needed functions for tile representation

--- src/handling/Corner.java
+++ src/handling/Corner.java
@@ -13,13 +13,51 @@ public class Corner {
   }
 
   /**
-   *
    * @param cornerInQuestion the corner that requires a new neighbor
-   * @param assumedTile the tile that is being checked to the corner
+   * @param assumedTile      the tile that is being checked to the corner
    * @return whether a tile can fit to the corner of another tile
    */
   public static boolean canTileFitCorner(Corner cornerInQuestion, Tile assumedTile) {
+    for (int i = 0; i < assumedTile.getCorners().length; i++) {
+      if(cornerInQuestion.getRepresentation().equalsIgnoreCase(rotateRepresentation(assumedTile.getCorners()[i].getRepresentation(), i + 2))) {
+        return true;
+      }
+    }
     return false;
   }
 
+  /**
+   * @param representation the representation that will be rotated
+   * @param n              the number of 90° rotations the corner will do
+   * @return a rotated representation of the original representation by n*90°
+   */
+  private static String rotateRepresentation(String representation, int n) {
+    String temp = representation;
+    for (int i = 0; i < /* could this be negative??, this will prevent it from being negative. */4 + (n % 4); i++) {
+      temp = reverseString(temp);
+    }
+    return temp;
+  }
+
+  /**
+   *
+   * @param input the text that has to be reversed
+   * @return the reverse of the input
+   */
+  private static String reverseString (String input) {
+    StringBuilder out = new StringBuilder();
+    for(int i = input.length(); i > 0; i--) {
+      out.append(input.charAt(i));
+    }
+    return out.toString();
+  }
+
+  /*
+  ----------
+  Getters and Setters
+  ----------
+   */
+  public String getRepresentation() {
+    return representation;
+  }
 }
